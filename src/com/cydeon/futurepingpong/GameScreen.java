e5cd9fd02927b4f9e50d8faee1fd57bfa4e07673
Joseph <josephoneill1997@gmail.com>
2014-01-06 19:01:06 -0500

    Added the Pause menu's update and present method
    I coded the update and present method for pause mode. There are
    currently three options: Reset Ball('s position), Resume(game), and
    Quit(game)

--- src/com/cydeon/futurepingpong/GameScreen.java
+++ src/com/cydeon/futurepingpong/GameScreen.java
@@ -22,6 +22,7 @@ import java.util.List;
 
 import javax.microedition.khronos.opengles.GL10;
 
+import android.R.color;
 import android.util.Log;
 
 import com.badlogic.androidgames.framework.Game;
@@ -43,8 +44,8 @@ public class GameScreen extends GLScreen {
 	Vector2 newV;
 	Ball2P ball;
 	Paddle2P paddle;
-	Rectangle pauseBounds;
-	public static Boolean screenIsTouched;
+	Rectangle pauseBounds, resetBallBounds, resumeBounds, quitBounds;
+	public static Boolean screenIsTouched, irbb, irb, iqb;
 	static final int GAME_READY = 0;
 	static final int GAME_RUNNING = 1;
 	static final int GAME_PAUSED = 2;
@@ -66,8 +67,14 @@ public class GameScreen extends GLScreen {
 		ball = world.ball;
 		paddle = world.userPaddle;
 		screenIsTouched = false;
+		irbb = false;
+		irb = false;
+		iqb = false;
 		pauseBounds = new Rectangle(World2P.WORLD_WIDTH - 4,
 				World2P.WORLD_HEIGHT - 5, 4, 4);
+		resetBallBounds = new Rectangle(World2P.WORLD_WIDTH / 2 - 18, 35, 40, 10);
+		resumeBounds = new Rectangle(World2P.WORLD_WIDTH / 2 - 12, 25, 24, 10);
+		quitBounds = new Rectangle(World2P.WORLD_WIDTH / 2 - 8, 15, 16, 10);
 		state = GAME_READY;
 		userScoreint = Ball2P.USER_SCORE_COUNTER;
 		cpuScoreInt = Ball2P.CPU_SCORE_COUNTER;
@@ -120,6 +127,10 @@ public class GameScreen extends GLScreen {
 			if (event.type == TouchEvent.TOUCH_UP) {
 				screenIsTouched = false;
 			}
+			
+			if (World2P.state == World2P.WORLD_STATE_GAME_OVER) {
+				state = GAME_OVER;
+			}
 
 		}
 		world.update(deltaTime, touchPoint);
@@ -132,7 +143,65 @@ public class GameScreen extends GLScreen {
 	}
 	
 	public void updatePaused() {
-		
+		List<TouchEvent> touchEvents = game.getInput().getTouchEvents();
+		int len = touchEvents.size();
+		for (int i = 0; i < len; i++) {
+			TouchEvent event = touchEvents.get(i);
+			if (event.type == TouchEvent.TOUCH_UP) {
+				touchPoint.set(event.x, event.y);
+				guiCam.touchToWorld(touchPoint);
+				if (OverlapTester.pointInRectangle(resetBallBounds, touchPoint)) {
+					renderer.changerbColor = false;
+					Ball2P.ballmode = Ball2P.BALL_GOAL_SCORED;
+					return;
+				}
+				if (OverlapTester.pointInRectangle(resumeBounds, touchPoint)) {
+					renderer.changerColor = false;
+					state = GAME_RUNNING;
+					return;
+				}
+				if (OverlapTester.pointInRectangle(quitBounds, touchPoint)) {
+					renderer.changeqColor = false;
+					World2P.ball.reset();
+					game.setScreen(new MainMenuScreen(game));
+					return;
+				}
+			}
+			if (event.type == TouchEvent.TOUCH_DOWN | event.type == TouchEvent.TOUCH_DRAGGED) {
+				touchPoint.set(event.x, event.y);
+				guiCam.touchToWorld(touchPoint);
+				if (OverlapTester.pointInRectangle(resetBallBounds, touchPoint)) {
+					renderer.changerbColor = true;
+					irbb = true;
+					return;
+				}
+				if (irbb && !OverlapTester.pointInRectangle(resetBallBounds, touchPoint)) {
+					irbb = false;
+					renderer.changerbColor = false;
+				}
+				if (OverlapTester.pointInRectangle(resumeBounds, touchPoint)) {
+					renderer.changerColor = true;
+					irb = true;
+					//Log.d("Pressed resume", "pressed resume");
+					return;
+				}
+				if (irb && !OverlapTester.pointInRectangle(resumeBounds, touchPoint)) {
+					irb = false;
+					renderer.changerColor = false;
+				}
+				if (OverlapTester.pointInRectangle(quitBounds, touchPoint)) {
+					renderer.changeqColor = true;
+					iqb = true;
+					return;
+				}
+				if (iqb && !OverlapTester.pointInRectangle(quitBounds, touchPoint)) {
+					iqb = false;
+					renderer.changeqColor = false;
+				}
+			}
+			
+
+		}
 	}
 	
 	public void updateGameOver() {
@@ -161,7 +230,17 @@ public class GameScreen extends GLScreen {
 	}
 	
 	public void presentPaused() {
+		GL10 gl = glGraphics.getGL();
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		gl.glEnable(GL10.GL_TEXTURE_2D);
 		
+		guiCam.setViewportAndMatrices();
+		gl.glEnable(GL10.GL_BLEND);
+		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT); // 5
+		gl.glClearColor(Settings.rgbBackground[0], Settings.rgbBackground[1], Settings.rgbBackground[2], 1.0f);
+		renderer.renderPaused();
 	}
 	
 	public void presentGameOver() {
@@ -172,6 +251,10 @@ public class GameScreen extends GLScreen {
 		GL10 gl = glGraphics.getGL();
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		gl.glEnable(GL10.GL_TEXTURE_2D);
+		
+		guiCam.setViewportAndMatrices();
+		gl.glEnable(GL10.GL_BLEND);
+		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
 
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT); // 5
 		gl.glClearColor(Settings.rgbBackground[0], Settings.rgbBackground[1], Settings.rgbBackground[2], 1.0f);
@@ -188,9 +271,7 @@ public class GameScreen extends GLScreen {
 		Assets.font.drawText(batcher, "CPU:" + Integer.toString(Ball2P.CPU_SCORE_COUNTER), (World2P.WORLD_WIDTH / 2), World2P.WORLD_HEIGHT - 2.5f, 2, 2.5f);
 		gl.glPopMatrix();
 		batcher.endBatch();
-		guiCam.setViewportAndMatrices();
-		gl.glEnable(GL10.GL_BLEND);
-		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+		
 	}
 
 	@Override
