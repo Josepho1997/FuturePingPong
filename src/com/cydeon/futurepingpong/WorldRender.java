1a937764a8f2988b24a9a30ccc9e786b39bed839
Joseph <josephoneill1997@gmail.com>
2014-01-06 19:03:45 -0500

    Added renderPaused method
    I added a method to render the pause menu. I also added some floats and
    booleans used in the pause menu.

--- src/com/cydeon/futurepingpong/WorldRender.java
+++ src/com/cydeon/futurepingpong/WorldRender.java
@@ -18,46 +18,95 @@
 
 package com.cydeon.futurepingpong;
 
+import java.util.Random;
+
 import javax.microedition.khronos.opengles.GL10;
 
-import com.badlogic.androidgames.framework.Game;
 import com.badlogic.androidgames.framework.gl.Camera2D;
 import com.badlogic.androidgames.framework.gl.SpriteBatcher;
 import com.badlogic.androidgames.framework.impl.GLGraphics;
 
 public class WorldRender {
 	static final float FRUSTUM_WIDTH = 80.0f; //
-	static final float FRUSTUM_HEIGHT = 48.0f; //Let goal be 3/7 of the height //Let Paddle be 2/3 of goal
+	static final float FRUSTUM_HEIGHT = 48.0f; // Let goal be 3/7 of the height
+												// //Let Paddle be 2/3 of goal
 	GLGraphics glGraphics;
 	World2P world;
 	Camera2D cam;
 	SpriteBatcher batcher;
+	Boolean changerbColor, changerColor, changeqColor;
+	Random ran;
+	float ranR, ranG, ranB;
 
-	public WorldRender(GLGraphics glGraphics, SpriteBatcher batcher, World2P world) {
+	public WorldRender(GLGraphics glGraphics, SpriteBatcher batcher,
+			World2P world) {
 		this.glGraphics = glGraphics;
 		this.world = world;
 		this.cam = new Camera2D(glGraphics, FRUSTUM_WIDTH, FRUSTUM_HEIGHT);
 		this.batcher = batcher;
-
+		changerbColor = false;
+		changerColor = false;
+		changeqColor = false;
+		ran = new Random();
+		ranR = ran.nextFloat();
+		ranG = ran.nextFloat();
+		ranB = ran.nextFloat();
 	}
-	
-	/*public void renderPreview() {
-		cam.setViewportAndMatrices();
-		renderObject();
-	}*/
+
+	/*
+	 * public void renderPreview() { cam.setViewportAndMatrices();
+	 * renderObject(); }
+	 */
 
 	public void render() {
 		cam.setViewportAndMatrices();
-		//renderBackground();
+		// renderBackground();
 		renderObjects();
 	}
 
-	/*public void renderBackground() {
-		batcher.beginBatch(Assets.background);
-		batcher.drawSprite(cam.position.x, cam.position.y, FRUSTUM_WIDTH,
-				FRUSTUM_HEIGHT, Assets.backgroundRegion);
+	public void renderPaused() {
+		cam.setViewportAndMatrices();
+		GL10 gl = glGraphics.getGL();
+		gl.glEnable(GL10.GL_BLEND);
+		gl.glEnable(GL10.GL_TEXTURE_2D);
+		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+		batcher.beginBatch(Assets.key);
+		gl.glPushMatrix();
+		if (!changerbColor) 
+			gl.glColor4f(1, 1, 1, 1);
+		else
+			gl.glColor4f(ranR, ranB, ranG, 1.0f);
+		Assets.font.drawText(batcher, "Reset Ball",
+				World2P.WORLD_WIDTH / 2 - 18, 40, 4, 5);
+		gl.glPopMatrix();
 		batcher.endBatch();
-	}*/
+		
+		batcher.beginBatch(Assets.key);
+		gl.glPushMatrix();
+		if (!changerColor)
+			gl.glColor4f(1, 1, 1, 1);
+		else
+			gl.glColor4f(ranR, ranB, ranG, 1.0f);
+		Assets.font.drawText(batcher, "Resume", World2P.WORLD_WIDTH / 2 - 12, 30, 4, 5);
+		gl.glPopMatrix();
+		batcher.endBatch();
+		
+		batcher.beginBatch(Assets.key);
+		gl.glPushMatrix();
+		if (!changeqColor)
+			gl.glColor4f(1, 1, 1, 1);
+		else
+			gl.glColor4f(ranR, ranB, ranG, 1.0f);
+		Assets.font.drawText(batcher, "Quit", World2P.WORLD_WIDTH / 2 - 8, 20, 4, 5);
+		gl.glPopMatrix();
+		batcher.endBatch();
+	}
+
+	/*
+	 * public void renderBackground() { batcher.beginBatch(Assets.background);
+	 * batcher.drawSprite(cam.position.x, cam.position.y, FRUSTUM_WIDTH,
+	 * FRUSTUM_HEIGHT, Assets.backgroundRegion); batcher.endBatch(); }
+	 */
 
 	public void renderObjects() {
 		GL10 gl = glGraphics.getGL();
@@ -68,73 +117,73 @@ public class WorldRender {
 		batcher.beginBatch(Assets.gameitems);
 		gl.glPushMatrix();
 		gl.glColor4f(0.3f, 0.3f, 0.6f, 0.2f);
-		batcher.drawSprite(1, World2P.WORLD_HEIGHT / 2, 2, World2P.GOAL_SIZE, Assets.paddle);
+		batcher.drawSprite(1, World2P.WORLD_HEIGHT / 2, 2, World2P.GOAL_SIZE,
+				Assets.paddle);
 		gl.glPopMatrix();
 		batcher.endBatch();
-		
+
 		batcher.beginBatch(Assets.gameitems);
-			gl.glPushMatrix();
-			gl.glColor4f(Settings.rgbBall[0], Settings.rgbBall[1], Settings.rgbBall[2], 1.0f);
-			batcher.drawSprite(world.ball.position.x, world.ball.position.y,
-					Ball2P.BALL_WIDTH, Ball2P.BALL_HEIGHT, Assets.ball);
-			gl.glPopMatrix();
-			batcher.endBatch();
-
-			batcher.beginBatch(Assets.gameitems);
-			gl.glPushMatrix();
-			gl.glColor4f(Settings.rgbPaddle1[0], Settings.rgbPaddle1[1], Settings.rgbPaddle1[2], 1.0f);
-			batcher.drawSprite(world.userPaddle.position.x, world.userPaddle.position.y, 
-					Paddle2P.PADDLE_WIDTH, Paddle2P.PADDLE_HEIGHT, Assets.paddle);
-			gl.glPopMatrix();
-			batcher.endBatch();
-		
-			batcher.beginBatch(Assets.gameitems);
-			gl.glPushMatrix();
-			gl.glColor4f(Settings.rgbPaddle2[0], Settings.rgbPaddle2[1], Settings.rgbPaddle2[2], 1.0f);
-			batcher.drawSprite(world.cpuPaddle.position.x, world.cpuPaddle.position.y,
-					PaddleAI2P.PADDLE_WIDTH, PaddleAI2P.PADDLE_HEIGHT, Assets.paddle);
-			gl.glPopMatrix();		//gl.glDisable(GL10.GL_BLEND);
-			batcher.endBatch();
-			
-			batcher.beginBatch(Assets.gameitems);
-			gl.glPushMatrix();
-			gl.glColor4f(Settings.rgbPause[0], Settings.rgbPause[1], Settings.rgbPause[2], 0.7f);
-			batcher.drawSprite(World2P.WORLD_WIDTH - 2, World2P.WORLD_HEIGHT - 2, Paddle2P.PADDLE_WIDTH / 2, Paddle2P.PADDLE_HEIGHT / 4, Assets.paddle);
-			batcher.drawSprite(World2P.WORLD_WIDTH - 4, World2P.WORLD_HEIGHT - 2, Paddle2P.PADDLE_WIDTH / 2, Paddle2P.PADDLE_HEIGHT / 4, Assets.paddle);
-			gl.glPopMatrix();
-			batcher.endBatch();
-			
-			
-	}
-	
-	/*public void renderObject() {
-		GL10 gl = glGraphics.getGL();
-		gl.glEnable(GL10.GL_BLEND);
-		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
-
-		batcher.beginBatch(Assets.items);
-		renderBall();
-		renderPaddleAi();
-		renderPaddleAi2();
+		gl.glPushMatrix();
+		gl.glColor4f(Settings.rgbBall[0], Settings.rgbBall[1],
+				Settings.rgbBall[2], 1.0f);
+		batcher.drawSprite(world.ball.position.x, world.ball.position.y,
+				Ball2P.BALL_WIDTH, Ball2P.BALL_HEIGHT, Assets.ball);
+		gl.glPopMatrix();
 		batcher.endBatch();
-		gl.glDisable(GL10.GL_BLEND);
-	}*/
-	
-	/*renderBall(gl);
-	renderPaddle(gl);
-	gl.glPushMatrix();
-	gl.glPopMatrix();
-	renderPaddleAi(gl);
-	gl.glPopMatrix();
-	gl.glPushMatrix();*/
 
-	
-	
-	/*private void renderPaddleAi2() {
-		batcher.drawSprite(World.paddleAi2.World2P.x, World.paddleAi2.World2P.y,
-				1.5f, 1.5f, Assets.paddle1);
-	}*/
+		batcher.beginBatch(Assets.gameitems);
+		gl.glPushMatrix();
+		gl.glColor4f(Settings.rgbPaddle1[0], Settings.rgbPaddle1[1],
+				Settings.rgbPaddle1[2], 1.0f);
+		batcher.drawSprite(world.userPaddle.position.x,
+				world.userPaddle.position.y, Paddle2P.PADDLE_WIDTH,
+				Paddle2P.PADDLE_HEIGHT, Assets.paddle);
+		gl.glPopMatrix();
+		batcher.endBatch();
 
+		batcher.beginBatch(Assets.gameitems);
+		gl.glPushMatrix();
+		gl.glColor4f(Settings.rgbPaddle2[0], Settings.rgbPaddle2[1],
+				Settings.rgbPaddle2[2], 1.0f);
+		batcher.drawSprite(world.cpuPaddle.position.x,
+				world.cpuPaddle.position.y, PaddleAI2P.PADDLE_WIDTH,
+				PaddleAI2P.PADDLE_HEIGHT, Assets.paddle);
+		gl.glPopMatrix(); // gl.glDisable(GL10.GL_BLEND);
+		batcher.endBatch();
 
+		batcher.beginBatch(Assets.gameitems);
+		gl.glPushMatrix();
+		gl.glColor4f(Settings.rgbPause[0], Settings.rgbPause[1],
+				Settings.rgbPause[2], 0.7f);
+		batcher.drawSprite(World2P.WORLD_WIDTH - 2, World2P.WORLD_HEIGHT - 2,
+				Paddle2P.PADDLE_WIDTH / 2, Paddle2P.PADDLE_HEIGHT / 4,
+				Assets.paddle);
+		batcher.drawSprite(World2P.WORLD_WIDTH - 4, World2P.WORLD_HEIGHT - 2,
+				Paddle2P.PADDLE_WIDTH / 2, Paddle2P.PADDLE_HEIGHT / 4,
+				Assets.paddle);
+		gl.glPopMatrix();
+		batcher.endBatch();
+
+	}
+
+	/*
+	 * public void renderObject() { GL10 gl = glGraphics.getGL();
+	 * gl.glEnable(GL10.GL_BLEND); gl.glBlendFunc(GL10.GL_SRC_ALPHA,
+	 * GL10.GL_ONE_MINUS_SRC_ALPHA);
+	 * 
+	 * batcher.beginBatch(Assets.items); renderBall(); renderPaddleAi();
+	 * renderPaddleAi2(); batcher.endBatch(); gl.glDisable(GL10.GL_BLEND); }
+	 */
+
+	/*
+	 * renderBall(gl); renderPaddle(gl); gl.glPushMatrix(); gl.glPopMatrix();
+	 * renderPaddleAi(gl); gl.glPopMatrix(); gl.glPushMatrix();
+	 */
+
+	/*
+	 * private void renderPaddleAi2() {
+	 * batcher.drawSprite(World.paddleAi2.World2P.x, World.paddleAi2.World2P.y,
+	 * 1.5f, 1.5f, Assets.paddle1); }
+	 */
 
 }
